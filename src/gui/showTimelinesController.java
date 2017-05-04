f5547eae8005c00eb4458f1bbec38cfe7003dfa6
AndersUOlsson <anders.u.olsson@gmail.com>
2017-05-04 12:02:37 +0200

    adds calender in zoomed timline

--- src/gui/showTimelinesController.java
+++ src/gui/showTimelinesController.java
@@ -1,10 +1,18 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -16,12 +24,30 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
+
+import cs_9roject.Timeline;
 import javafx.scene.paint.Color;
 
 
+
+
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import javafx.util.Callback;
+
+
+
+
 public class showTimelinesController {
+	public int timelineId = 0;
 	
 	public ScrollPane primaryScrollpane;
 
@@ -91,9 +117,13 @@ public class showTimelinesController {
 		
 		Line timeLine = new Line(0, 50, size, 50);
 		timeLine.setStrokeWidth(5);
+		
 		timeLine.setOnMouseClicked(e ->{
 			this.scrollBox.getChildren().add(zoomedTimeline());
 		});
+		
+		
+		
 		
 		return timeLine;
 	}
@@ -132,22 +162,22 @@ public class showTimelinesController {
 		return pane;
 	}
 	
-	public Line zommedHline(int size ){		//Making non clickable horizontal line
+	public Line zommedHline(int size){		//Making non clickable horizontal line
 		
 		Line timeLine = new Line(0, 50, size, 50);
 		
 		
-		/*
-		 * Thus not work yet
-		 * Color color = Main.project.getTimeline(1).getEvent(0).getColor();
-		 * timeLine.setFill(color);
-		 */
+			scrollBox.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				public void handle(MouseEvent e) {
+					showInfoByMonth(0, 2017, 5);
+				}
+			});
+			primaryScrollpane.setContent(scrollBox);
+			Stage stage = (Stage) doneButton.getScene().getWindow();
+			stage.close();
+			
 	
-		 
-		
-		
-		
-		
+
 		return timeLine;
 	}
 	
@@ -245,6 +275,156 @@ public class showTimelinesController {
 		  pane.getChildren().add(blank);
 		  
 		  return pane;		
+	}
+	
+	/**
+	 * 
+	 * @param timelineId     the id of which TimeLine you clicked.
+	 * @param year           which year you clicked 
+	 * @param month			which month you cliced.
+	 */
+	public void showInfoByMonth (final int timelineId, int year, int month) {
+		FXMLLoader loader = new FXMLLoader();
+		final DatePicker startDatePicker = new DatePicker();
+		startDatePicker.setValue(LocalDate.of(year, month, 1));
+		
+		this.timelineId = timelineId;
+
+		/**
+		 * set the style relation about the cellEdit of the DaterPicker
+		 */
+		final Callback<DatePicker, DateCell> dayCellFactory = new Callback<DatePicker, DateCell>() {
+			public DateCell call(final DatePicker datePicker) {
+				return new DateCell() {
+					@Override
+					public void updateItem(LocalDate item, boolean empty) {
+						super.updateItem(item, empty);
+						List<Timeline> timelines = Main.project.getTimelines();
+
+						for (Timeline t : timelines) {	
+							if (t.getTimelineId() == timelineId) {		//get the infomation of the Timeline you clicked
+								List<cs_9roject.Event> events = t.getEvents();
+								for (cs_9roject.Event e : events) {		//get all the events of the the timeline
+									LocalDateTime startTime = e.getStartTime();	
+									LocalDateTime endTime = e.getEndTime();
+									
+									String startMonth = startTime.getMonthValue() / 10 > 0 ? "" + startTime.getMonthValue() : "0" + startTime.getMonthValue();
+									String startDay = startTime.getDayOfMonth() / 10 > 0 ? "" + startTime.getDayOfMonth() : "0" + startTime.getDayOfMonth();
+									
+									String startTimeStr = startTime.getYear() + "-" + startMonth + "-" + startDay;
+									String endTimeStr = "";
+									
+									if (endTime == null) {		// if the end time is not duration
+										endTimeStr = startTimeStr;
+									} else {
+										String endMonth = endTime.getMonthValue() / 10 > 0 ? "" + endTime.getMonthValue() : "0" + endTime.getMonthValue();
+										String endDay = endTime.getDayOfMonth() / 10 > 0 ? "" + endTime.getDayOfMonth() : "0" + endTime.getDayOfMonth();
+										endTimeStr = endTime.getYear() + "-" + endMonth + "-" + endDay;
+									}
+									
+									String itemMonth = item.getMonthValue() / 10 > 0 ? "" + item.getMonthValue() : "0" + item.getMonthValue();
+									String itemDay = item.getDayOfMonth() / 10 > 0 ? "" + item.getDayOfMonth() : "0" + item.getDayOfMonth();
+									String itemStr = item.getYear() + "-" + itemMonth +"-" + itemDay;
+
+									if (itemStr.compareTo(startTimeStr) >= 0 && itemStr.compareTo(endTimeStr) <= 0) {
+										Color c = e.getColor();
+										String colorStr = c.toString().substring(4);
+										setStyle("-fx-background-color: #" + colorStr);			//set the color of the cell
+										setTooltip(new Tooltip(e.getTitle()));
+									}
+								}
+
+								break;
+							}
+						}
+					}	
+				};
+			}
+		};
+		
+		startDatePicker.setOnAction(new EventHandler<ActionEvent>() {		//add a action event to the DaterPicker.
+			public void handle(ActionEvent e) {			
+				String handlerTime = startDatePicker.getValue().toString();
+				
+				boolean isExist = false;
+				
+				for (Timeline t : Main.project.getTimelines()) {
+					if (t.getTimelineId() == timelineId) {				
+						List<cs_9roject.Event> events = t.getEvents();
+						for (cs_9roject.Event event : events) {
+							LocalDateTime startTime = event.getStartTime();
+							LocalDateTime endTime = event.getEndTime();
+							
+							String startMonth = startTime.getMonthValue() / 10 > 0 ? "" + startTime.getMonthValue() : "0" + startTime.getMonthValue();
+							String startDay = startTime.getDayOfMonth() / 10 > 0 ? "" + startTime.getDayOfMonth() : "0" + startTime.getDayOfMonth();
+							
+							String startTimeStr = startTime.getYear() + "-" + startMonth + "-" + startDay;
+							String endTimeStr = "";
+							
+							if (endTime == null) {
+								endTimeStr = startTimeStr;
+							} else {
+								String endMonth = endTime.getMonthValue() / 10 > 0 ? "" + endTime.getMonthValue() : "0" + endTime.getMonthValue();
+								String endDay = endTime.getDayOfMonth() / 10 > 0 ? "" + endTime.getDayOfMonth() : "0" + endTime.getDayOfMonth();
+								endTimeStr = endTime.getYear() + "-" + endMonth + "-" + endDay;
+							}
+							
+							if (handlerTime.compareTo(startTimeStr) >=0 && handlerTime.compareTo(endTimeStr) <= 0) {
+								isExist = true;
+								try {
+									FXMLLoader loader = new FXMLLoader();
+									loader.setLocation(Main.class.getResource("eventHandler.fxml"));		//show the eventHandler GUI by the infomation of the time you clicked
+									Pane showEventHandler = loader.load();
+									eventHandlerController handler = loader.getController();
+									//handler.getStartTextField().setValue(startTime.toLocalDate());
+									if (endTime != null)
+										handler.getEndValue().setValue(endTime.toLocalDate());
+									//handler.getDescription().setText(event.getDescription());
+								//	handler.getNameEvent_textField().setText(event.getTitle());
+									
+									Stage stage2 = new Stage();
+									stage2.setScene(new Scene(showEventHandler));  
+									stage2.setTitle("EventHandler");
+									stage2.show();
+								} catch (IOException e1) {
+									e1.printStackTrace();
+								}		
+								break;
+							} 
+						}
+						break;
+					}
+				}
+				
+				if (!isExist) {
+					try {
+						FXMLLoader loader = new FXMLLoader();
+						loader.setLocation(Main.class.getResource("eventHandler.fxml"));
+						Pane showEventHandler = loader.load();
+						eventHandlerController handler = loader.getController();
+						
+						Stage stage2 = new Stage();
+						stage2.setScene(new Scene(showEventHandler));  
+						stage2.setTitle("EventHandler");
+						stage2.show();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+				}
+			}
+		});
+
+		startDatePicker.setDayCellFactory(dayCellFactory);
+		DatePickerSkin datePickerSkin = new DatePickerSkin(startDatePicker);
+		Node popupContent = datePickerSkin.getPopupContent();
+		popupContent.applyCss();
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(popupContent);
+
+		Stage stage2 = new Stage();
+		stage2.setScene(new Scene(borderPane, 400, 400)); 
+		stage2.setTitle("EventHandler");
+		stage2.show();
 	}
 	
 }
