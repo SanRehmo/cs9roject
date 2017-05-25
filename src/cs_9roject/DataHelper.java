ff7c47c26d05e40ff7e13ad6b003456e8730d551
Christian <christian_rehm@gmx.net>
2017-05-25 20:37:45 +0200

    Big performance improvements

---
+++ src/cs_9roject/DataHelper.java
@@ -0,0 +1,23 @@
+package cs_9roject;
+
+/**
+ * Created by Christian on 25.05.2017.
+ */
+public class DataHelper {
+
+    int id;
+    String name;
+
+    public DataHelper(int ID, String Name) {
+        id = ID;
+        name = Name;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
